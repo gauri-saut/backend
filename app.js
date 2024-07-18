43414be7208a37c2b24d7c861f6641a67f60260d
krishna <knikam2025@gmail.com>
2024-07-18 17:56:54 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors'
+import { course } from './db.js';
+const app = express();
+
+import 'dotenv/config';
+
+
+dotenv.config();
+app.use(cors());
+
+
+const port = process.env.port;
+const hostname =  process.env.hostnames;
+
+
+ app.get("/", (req,res)=> {
+
+
+
+  
+    res.send(course)
+ });
+
+
+ app.get("/krishna", (req, res)=> {
+    res.send("hello krishna")
+ })
+
+ app.get("/home", (req, res)=> {
+    console.log("home")
+ })
+ 
+ app.listen(port, ()=> {
+    console.log(`server is working http://${hostname}:${port}`)
+ });
+
+
+
+
+
---
+++ db.js
@@ -0,0 +1,19 @@
+export const course= [
+    {
+       id:1,
+       courseName: "MERN",
+
+    },
+
+    {
+       id:2,
+       courseName: "MEAN",
+
+    },
+    {
+       id:2,
+       courseName: "MEAN",
+
+    },
+ ]
+ 
